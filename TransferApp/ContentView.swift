3a5b82b7138b64a641c071e02f5e510eb5906321
oussama litniti <oussama.litniti@babelgroup.com>
2026-06-15 19:17:04 +0100

    Initial Commit

---
+++ TransferApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TransferApp
+//
+//  Created by Oussama Litniti on 15/6/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TransferApp/TransferAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransferAppApp.swift
+//  TransferApp
+//
+//  Created by Oussama Litniti on 15/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct TransferAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
